3b4365c282f4d27fd6c0bfbf3e4a4ae3be8e9f5d
Noah Mekonnen <noahmekonnen0@gmail.com>
2023-12-27 20:44:20 -0500

    commenting stuff out

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -16,16 +16,16 @@ session['score'] = 0
 
 @app.route('/')
 def welcome():
-    result = request.form.get('result')
-    if (result):
-        if result == "ok":
-            return render_template('result.html', resp = "Valid word")
+    # result = request.form.get('result')
+    # if (result):
+    #     if result == "ok":
+    #         return render_template('result.html', resp = "Valid word")
         
-        elif result == "not-on-board":
-            return render_template('result.html', resp = "Invalid word")
+    #     elif result == "not-on-board":
+    #         return render_template('result.html', resp = "Invalid word")
         
-        else:
-            return render_template('result.html', resp = "This word doesn't exist")
+    #     else:
+    #         return render_template('result.html', resp = "This word doesn't exist")
     new_board = boggle_game.make_board()
     session['new_board'] = new_board
     return render_template('base.html', new_board = new_board)
@@ -35,11 +35,11 @@ def check_guess():
     guess_in_words = guess in boggle_game.words
     guess = request.form.get('guess')
     valid_word = boggle_game.check_valid_word(new_board, guess)
-    if guess_in_words:
-        if valid_word:
-            # {"result": "ok"}
-        else:
-            # {"result": "not-on-board"}
-    else:
-        # {"result": "not-a-word"}
+    # if guess_in_words:
+    #     if valid_word:
+    #         # {"result": "ok"}
+    #     else:
+    #         # {"result": "not-on-board"}
+    # else:
+    #     # {"result": "not-a-word"}
     return redirect('/')
