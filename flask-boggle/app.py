7f360c00c45965bc22b57fcb23ededf9f270e58c
Noah Mekonnen <noahmekonnen0@gmail.com>
2024-01-05 00:32:26 -0500

    adjustments

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -1,6 +1,7 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, session, redirect
+from flask import Flask, request, render_template, session, redirect, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
@@ -12,17 +13,29 @@ debug = DebugToolbarExtension(app)
 app.debug = True
 
 boggle_game = Boggle()
-# session['score'] = 0
 
+session['num_of_games'] = 0
+
+"""Starts a Boggle Game"""
 @app.route('/')
 def welcome():
     new_board = boggle_game.make_board()
     session['new_board'] = new_board
+    session['num_of_games'] +=1
     return render_template('base.html', new_board = new_board)
 
-@app.route('/guesses', methods = ['POST'])
+"""Starts Game and accepts post request of score"""
+@app.route('/', methods = ['POST'])
+def post_request():
+    print(request.json, "home")
+    new_board = boggle_game.make_board()
+    session['new_board'] = new_board
+    session['num_of_games'] +=1
+    return render_template('base.html', new_board = new_board)
+
+"""Checks if word is valid"""
+@app.route('/check-word', methods = ['POST'])
 def check_guess():
-    guess = request.args["guess"]
-    guess_in_words = guess in boggle_game.words
+    guess = request.json['params']['guess'] 
     valid_word = boggle_game.check_valid_word(session['new_board'], guess)
-    return redirect('/')
+    return jsonify({'result': valid_word})
--- flask-boggle/static/stuff.js
+++ flask-boggle/static/stuff.js
@@ -1,14 +1,72 @@
+"use strict";
+
 const form = $("form")
+const button = $("button")
+const input = $("input")
+const label = $("label")
+const body = $("body")
+const result = $("<h3></h3>")
+const startButton = $("<button></button>").text("Start Game")
 
+body.append(startButton)
+startButton.hide()
 
-async function getRequest(e){
-    guess = $('#guess')[0].value
-    response = await axios.post('/guesses',{params:{guess: guess}});
-    console.log(response)
+let score = 0
+
+let listOfWords = []
+
+// post request for button
+async function postRequest(){
+    const guess = $('#guess')[0].value
+    
+    if (!(guess in listOfWords)){
+        const response = await axios.post("/check-word",{params:{guess: guess}},{headers: {'Content-Type': 'application/json'}});
+        // making response based on validity of word
+        if (response.data.result == "ok"){
+            result.text("This is a valid word")
+            score += guess.length
+            $("#score").text(score)
+        }
+        else if (response.data.result == "not-a-word"){
+            result.text("This is not a word")
+        }
+        else{
+            result.text("This is not on the board")
+        }
+        body.append(result)
+    }
+    listOfWords.push(guess)
 }
 
+// form functionality
 form.on('submit', function buttonRequest(e){
     e.preventDefault();
-    getRequest();
+    console.log("online!")
+    postRequest();
+    form[0].reset()
 })
+
+// start button functionality
+startButton.on("click", function startGame(e){
+    e.preventDefault()
+    form.show()
+    button.show()
+    label.show()
+    input.show()
+    startButton.hide()
+    setTimeout(gameOver, 5000)
+})
+
+// ending the game
+async function gameOver(){
+    form.hide()
+    button.hide()
+    label.hide()
+    input.hide()
+    await axios.post("/",{params:{score: score}})
+    startButton.show()
+}
+
+// stop guessing after 60 seconds
+setTimeout(gameOver, 5000)
 
